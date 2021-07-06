151ac6adc821268cb30889b616e642c7aa104ceb
Stephanie Fernandes <msfern.dev@gmail.com>
2021-07-05 21:33:48 -0300

    feat: configure storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,16 @@
+import { Story, Meta } from '@storybook/react'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main
-}
+} as Meta
 
-export const Basic = () => <Main />
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Primary = Basic.bind({})
+
+Primary.args = {
+  title: 'fkn',
+  description: 'Button'
+}
